f0ce801fc5cfeed30c4e155f070d7f1cf54a956f
teammcarthur <chrism.23092@obhs.school.nz>
2026-08-20 20:42:57 +1200

    initial code
    imported libraries, finished program description, and started main routine

--- id_number.py
+++ id_number.py
@@ -2,14 +2,19 @@
     Author: Chris M
     Version: 1.0
     Date: 20/08/26
-    Description: 
+    Description: Improved ID number program
 '''
 
 
 #-----Libraries-----
-
+import random
+import time
+import string
+import os
 
 #-----Functions-----
 
 
 #-----Main Routine-----
+if(__name__ == "__main__"):
+    pass
