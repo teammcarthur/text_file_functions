e4818866fabfda2abeb5eb7b252d646f996fc342
teammcarthur <chrism.23092@obhs.school.nz>
2026-08-23 15:41:05 +1200

    fixed errors

--- id_number.py
+++ id_number.py
@@ -29,21 +29,21 @@ if(__name__ == "__main__"):
     code = 0
     while(True):
         first_name = input("Please enter your first name")
-        if(len(first_name) < 2 or len(first_name) > 20 or not first_name.isalpha):
+        if(len(first_name) < 2 or len(first_name) > 20 or not first_name.isalpha()):
             print("Please enter a valid name")
             continue
         else:
             break
     while(True): #This loop validates the users input for last name
         last_name = input("Please enter your last name")
-        if(len(last_name) < 2 or len(last_name) > 20 or not last_name.isalpha):
+        if(len(last_name) < 2 or len(last_name) > 20 or not last_name.isalpha()):
             print("Please enter a valid name")
             continue
         else:
             break
     year = random.randint(2025, 2050) #Stores a random number from 2025 to 2050
-    string_slice(last_name, first_name, year) # calls the string slice function
-    unique_code(code) # calls the unique code function
+    combined_name = string_slice(last_name, first_name, year) # calls the string slice function
+    code = unique_code(code) # calls the unique code function
     username = combined_name + str(code) # concatenates the combined name and the unique code
     print(username)
     gmail_login = username + "@gmail.com" # adds @gmail.com to the end of the username to create a gmail login
